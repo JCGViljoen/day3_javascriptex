8d140812c07c17fd6881fc850c3967619e8b9320
JCGViljoen <jcgviljone51@gmail.com>
2023-05-17 09:45:59 +0200

    data type exercise

---
+++ jsDayThree/script/code.js
@@ -0,0 +1,29 @@
+// create variable the reassign it
+let firstName = ('Jason');
+firstName= (7);
+console.log(typeof firstName); 
+
+// Boolean data type
+let surname = (2);
+console.log(firstName > surname);
+
+// undefined data type
+let middleName;
+console.log(typeof middleName)
+
+// Null
+let name = null;
+console.log(name);
+
+// object
+let object = null;
+console.log(typeof object);
+
+// function
+prompt('Enter your Name')
+console.log(typeof prompt);
+
+// bigInt
+let bigNumber = 5045144551000000n;
+console.log(typeof bigNumber);
+
